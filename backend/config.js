184d143f2106fa3b357b2546d9f78cb3bbe4913f
Divyanshii-Rajput <divyanshirajput02003@gmail.com>
2025-03-19 10:46:41 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongodbURL ='mongodb+srv://divyanshirajput02003:9H2JFou7HAklMWkE@bookstoremern.qpxmi.mongodb.net/book';
+// export const mongodbURL =
+//   'mongodb://localhost:27017/book';
+  // 'mongodb+srv://divyanshirajput02003:9H2JFou7HAklMWkE@bookstoremern.qpxmi.mongodb.net/book';
+  // mongodb+srv://divyanshirajput02003:9H2JFou7HAklMWkE@bookstoremern.qpxmi.mongodb.net/book
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
